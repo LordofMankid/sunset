73c204e0a017cb714a54970f43e6adaee3f1bd23
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2023-06-07 16:40:46 -0400

    connected charData to damage calculation

--- assets/_Scripts/projectileControl.cs
+++ assets/_Scripts/projectileControl.cs
@@ -18,13 +18,14 @@ public class projectileControl : MonoBehaviour
     private Vector2 startLocation;
     
     private Rigidbody2D projectile;
-
+    private CharacterData shooter;
     private GameObject target; // passed in when called, I feel like this should be in some sort of constructor?
 
     // Start is called before the first frame update
 
-    public void setTarget(GameObject objectToFireAt)
+    public void setVariables(GameObject objectToFireAt, CharacterData objectFiringFrom)
     {
+        shooter = objectFiringFrom;
         target = objectToFireAt;
 
     }
@@ -33,14 +34,17 @@ public class projectileControl : MonoBehaviour
     {
         projectile = GetComponent<Rigidbody2D>();
         startLocation = projectile.position;
-
+        damage += shooter.damage;
+       
 
         // TODO (eventually) make target selection a variable that you can set for ally units and yourself (fire on closest target, highest health, etc)
         // also figure out a way that doesn't involve an O(n) every time you fucking fire [maybe overhead isn't that bad with < 1000 targets]
-            Vector2 targetVector = (Vector2)target.transform.position - projectile.position; // find the vector in between target and the curren position
-            projectile.velocity = targetVector.normalized * speed;  // moves towards player's direction
-        } 
-        
+           Vector2 targetVector = (Vector2)target.transform.position - projectile.position; // find the vector in between target and the curren position
+           // TODO: make trig to have bullet point in direction it's going in
+           projectile.velocity = targetVector.normalized * speed;  // moves towards player's direction
+       }
+
+    
 
 
     private void OnTriggerEnter2D(Collider2D hitInfo)
@@ -49,9 +53,10 @@ public class projectileControl : MonoBehaviour
         {
             //Debug.Log(hitInfo.name + " takes damage");
             CharacterControl character = hitInfo.GetComponent<CharacterControl>();
-
+            
             if(character != null)
             {
+                
                 character.TakeDamage(damage);
             }
             Destroy(gameObject); // disappear once hit
@@ -59,6 +64,7 @@ public class projectileControl : MonoBehaviour
         }
     }
 
+    
     private void Update()
     {
         if(Vector2.Distance(projectile.position, startLocation) > range)
